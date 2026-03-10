3d19fc7598fc6d2e49e05cf7bbd47187ef3cf40d
Quadeatschool <quadehafen@byui.edu>
2026-03-09 18:15:57 -0600

    Create Compliment.py

---
+++ Compliment.py
@@ -0,0 +1,31 @@
+from typing import Dict, TypedDict
+from langgraph.graph import StateGraph
+
+class AgentState (TypedDict):
+    message : str 
+
+def personal_node(state: AgentState) -> AgentState:
+    """An ai that is personal lol"""
+    state['message'] = "Hey + state['message'] +, love you munches"
+
+    return state
+
+
+graph = StateGraph(AgentState)
+
+graph.add_node("receptrionist", personal_node)
+
+graph.set_entry_point("receptrionist")
+graph.set_finish_point("receptrionist")
+
+app = graph.compile()
+
+from IPython.display import Image, display
+display(Image(app.get_graph().draw_mermaid_png))
+
+
+
+
+
+
+
